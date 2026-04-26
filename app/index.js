93115f5e9af76241da25e0c6bc1f6e814ba195d6
nuthchanrithea654-hub <nuthchanrithea654@gmail.com>
2026-04-26 22:26:56 +0700

    Developer 1 updates home API message

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("AUPP LMS DevOps API - Main Version");
+  res.send("AUPP LMS DevOps API - Updated by Developer 1");
 });
 
 app.get("/courses", (req, res) => {
