407a8d582c5e1ea3c26260d56b84164f32950953
nuthchanrithea654-hub <nuthchanrithea654@gmail.com>
2026-04-26 22:20:36 +0700

    Initial AUPP LMS backend API

---
+++ app/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AUPP LMS DevOps API - Main Version");
+});
+
+app.get("/courses", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      course: "Cloud Automation"
+    },
+    {
+      id: 2,
+      course: "DevOps"
+    }
+  ]);
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`AUPP LMS API running on port ${PORT}`);
+});
